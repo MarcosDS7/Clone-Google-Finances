97613abbf9e47587bf77df677dd79e9395d3d2b1
Marcos Vinícius Rodrigues de Souza <marcossouza.0ds@gmail.com>
2022-01-30 16:32:30 -0300

    Preparação de ambiente

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title =
+    'Google Finance: preços da bolsa de valores, cotações em tempo real e notícias sobre o mercado financeiro';
+}
