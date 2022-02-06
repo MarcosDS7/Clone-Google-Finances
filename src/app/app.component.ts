ae99c03f52db39368dc6301f44971ac89fd18365
Marcos Vinícius Rodrigues de Souza <marcossouza.0ds@gmail.com>
2022-02-06 18:08:51 -0300

    Adição do toaster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title =
     'Google Finance: preços da bolsa de valores, cotações em tempo real e notícias sobre o mercado financeiro';
+
+  constructor(private toastrService: ToastrService) {}
+
+  ngOnInit(): void {
+    this.toastrService.success(
+      '<i class="bi bi-currency-dollar text-success"></i>',
+      'Preços da bolsa de valores, cotações em tempo real e notícias sobre o mercado financeiro',
+      {
+        progressBar: true,
+      }
+    );
+  }
 }
